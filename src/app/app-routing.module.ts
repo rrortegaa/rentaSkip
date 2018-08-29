a4c38170796106e8692db99d6f9a316ac5b45d71
rrortegaa <rrortegaa@gmail.com>
2018-08-29 16:11:06 -0500

    commit del proyecto

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { PrincipalComponent } from './principal/principal.component';
+
+const routes: Routes = [
+  {
+    path: 'principal',
+    component: PrincipalComponent
+  },
+  { path: '', redirectTo: '/principal', pathMatch:'full' }
+]
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes)],
+  exports: [ RouterModule]
+})
+
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
+import { PrincipalComponent } from './principal/principal.component';
+import { AppRoutingModule } from './/app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    FooterComponent,
+    PrincipalComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
